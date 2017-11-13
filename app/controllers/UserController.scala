a31923467171c123e1f8a4dfe4220034e1c5aa1b
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-11-13 16:08:11 -0200

    Iniciando a implementação dos controllers.

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,101 @@
+package controllers
+
+import java.util.Date
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.db.DBApi
+
+import anorm._
+import anorm.{ Macro, RowParser }
+
+import models.User
+
+@Singleton
+class UserController @Inject()(cc: ControllerComponents, dbapi: DBApi) extends AbstractController(cc) {
+
+  val db = dbapi.database("default")
+  val parser: RowParser[User] = Macro.namedParser[User]
+
+  def listAll = Action { implicit request: Request[AnyContent] =>
+    db.withConnection { implicit c =>
+      val result: List[User] = SQL("select * from User;").as(parser.*)
+      Ok(Json.toJson(result))
+    }
+  }
+
+  def listOne(id: Long) = Action { implicit request: Request[AnyContent] =>
+    db.withConnection { implicit c =>
+      val List(result): List[User] = SQL("select * from User where id = " + id + ";").as(parser.*)
+      Ok(Json.toJson(result))
+    }
+  }
+
+  def create = Action(parse.json) { implicit request: Request[JsValue] =>
+
+    db.withConnection { implicit c =>
+
+      val obj = request.body
+      val u = User(
+        None,
+        (obj \ "email").as[String],
+        (obj \ "password").as[String],
+        (obj \ "token").as[Int],
+        (obj \ "createdAt").as[Date],
+        (obj \ "updatedAt").as[Date]
+      )
+
+      SQL("insert into User(email, password, token, createdAt, updatedAt) " +
+        "values('" +
+        u.email + "', '" +
+        u.password + "', " +
+        u.token + ", " +
+        u.createdAt + ", " +
+        u.updatedAt + ");").execute()
+
+      Ok(Json.parse("{\"msg\": \"success\"}"))
+
+    }
+
+  }
+
+  def update = Action(parse.json) { implicit request: Request[JsValue] =>
+
+    db.withConnection { implicit c =>
+
+      val obj = request.body
+      val u = User(
+        Some((obj \ "id").as[Long]),
+        (obj \ "email").as[String],
+        (obj \ "password").as[String],
+        (obj \ "token").as[Int],
+        (obj \ "createdAt").as[Date],
+        (obj \ "updatedAt").as[Date]
+      )
+
+      val Some(id) = u.id
+
+      SQL("update User set " +
+        "email = '" + u.email + "', " +
+        "password = '" + u.password + "', " +
+        "token = " + u.token + ", " +
+        "createdAt = '" + u.createdAt + "', " +
+        "updatedAt = '" + u.updatedAt + "' " +
+        "where id = " + id + ";").execute()
+
+      Ok(Json.parse("{\"msg\": \"success\"}"))
+
+    }
+
+  }
+
+  def delete(id: Long) = Action { implicit request: Request[AnyContent] =>
+    db.withConnection { implicit c =>
+      SQL("delete from User where id = " + id + ";").execute()
+      Ok(Json.parse("{\"msg\": \"success\"}"))
+    }
+  }
+
+}
