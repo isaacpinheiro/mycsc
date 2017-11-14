b35284d74528de521ec0813f470fd496d3b725ce
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-11-14 17:55:07 -0200

    Pequena correção na indentação

--- app/utils/MySQLDateFormat.scala
+++ app/utils/MySQLDateFormat.scala
@@ -4,18 +4,18 @@ import java.util.Date
 
 object MySQLDateFormat {
 
-    def format(d: Date): String = {
+  def format(d: Date): String = {
 
-        val year = (d.getYear() + 1900).toString
-        val month = (d.getMonth() + 1).toString
-        val date = d.getDate().toString
-        val hours = d.getHours().toString
-        val minutes = d.getMinutes().toString
-        val seconds = d.getSeconds().toString
+    val year = (d.getYear() + 1900).toString
+    val month = (d.getMonth() + 1).toString
+    val date = d.getDate().toString
+    val hours = d.getHours().toString
+    val minutes = d.getMinutes().toString
+    val seconds = d.getSeconds().toString
 
-        val res = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
-        res
+    val res = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
+    res
 
-    }
+  }
 
 }
