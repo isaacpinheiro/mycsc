ee9dfe9b8c2dac28b0f20bca654e76e51c223028
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-12-05 18:00:24 -0200

    Implementando a validação do formulário de cadastro de empresas.

--- src/main/resources/static/js/cadastrarempresa.js
+++ src/main/resources/static/js/cadastrarempresa.js
@@ -4,8 +4,61 @@ $(document).ready(function() {
 
     $('#sign_up_btn').click(function() {
 
-        $('#alertMsg').html('Teste');
-        $('#alertModal').modal();
+        var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+        var email = $('#sign_up_email').val();
+        var senha = $('#sign_up_senha').val();
+        var confSenha = $('#sign_up_conf_senha').val();
+        var nome = $('#sign_up_nome').val();
+        var cnpj = $('#sign_up_cnpj').val();
+        var pais = $('#sign_up_pais').val();
+
+        if (email === '' || email === null) {
+
+            $('#alertMsg').html('Por favor, informe o seu E-Mail.');
+            $('#alertModal').modal();
+
+        } else if (!filter.test(email)) {
+
+            $('#alertMsg').html('Por favor, informe um E-Mail válido.');
+            $('#alertModal').modal();
+
+        } else if (senha === '' || senha === null) {
+
+            $('#alertMsg').html('Por favor, informe a sua senha.');
+            $('#alertModal').modal();
+
+        } else if (confSenha === '' || confSenha === null) {
+
+            $('#alertMsg').html('Por favor, confirme a sua senha.');
+            $('#alertModal').modal();
+
+        } else if (confSenha !== senha) {
+
+            $('#alertMsg').html('A confirmação está diferente da senha.');
+            $('#alertModal').modal();
+
+        } else if (nome === '' || nome === null) {
+
+            $('#alertMsg').html('Por favor, informe o nome da sua empresa.');
+            $('#alertModal').modal();
+
+        } else if (cnpj === '' || cnpj === null) {
+
+            $('#alertMsg').html('Por favor, informe o cnpj da sua empresa.');
+            $('#alertModal').modal();
+
+        } else if (pais === '' || pais === null) {
+
+            $('#alertMsg').html('Por favor, informe o país de origem da sua empresa.');
+            $('#alertModal').modal();
+
+        } else {
+
+            $('#alertMsg').html('Success');
+            $('#alertModal').modal();
+
+        }
 
     });
 
