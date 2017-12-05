19416a60dc43ee63bd9d3bad2313952d95790e24
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-12-05 19:33:10 -0200

    Implementando o cadastro de usuários na API.

--- src/main/java/br/isaac/mycsc/controller/EnterpriseUserController.java
+++ src/main/java/br/isaac/mycsc/controller/EnterpriseUserController.java
@@ -1,5 +1,7 @@
 package br.isaac.mycsc.controller;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.isaac.mycsc.repository.EnterpriseUserRepository;
+import br.isaac.mycsc.repository.UserRepository;
 import br.isaac.mycsc.model.EnterpriseUser;
+import br.isaac.mycsc.model.User;
+import br.isaac.mycsc.util.MyHash;
 
 @RestController
 public class EnterpriseUserController {
 
     @Autowired
     private EnterpriseUserRepository repository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping(value="/api/enterpriseuser/{id}", method=RequestMethod.GET, produces="application/json")
     public @ResponseBody EnterpriseUser listOne(@PathVariable("id") String id) {
@@ -30,6 +38,36 @@ public class EnterpriseUserController {
 
     @RequestMapping(value="/api/enterpriseuser", method=RequestMethod.POST, consumes="application/json")
     public @ResponseBody String insert(@RequestBody EnterpriseUser obj) {
+
+        Date now = new Date();
+
+        if (obj.getId() == null) {
+
+            User u = userRepository.findByEmail(obj.getUser().getEmail());
+
+            if (u != null) {
+                return "{\"msg\": \"Já existe um usuário cadastrado com esse E-Mail.\"}";
+            }
+
+            u = new User();
+            u.setEmail(obj.getUser().getEmail());
+            u.setCreatedAt(now);
+            u.setUpdatedAt(now);
+            u.setToken(MyHash.generateToken());
+            u.setPassword(MyHash.generate(obj.getUser().getPassword(), u.getToken()));
+            userRepository.save(u);
+
+            u = userRepository.findByEmail(obj.getUser().getEmail());
+            obj.setUser(u);
+            obj.setCreatedAt(now);
+            obj.setUpdatedAt(now);
+
+        } else {
+
+            obj.setUpdatedAt(now);
+
+        }
+
         repository.save(obj);
         return "{\"msg\": \"success\"}";
     }
