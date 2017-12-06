2cff4442090ec7621488976b6242c82f44158816
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-12-06 03:10:00 -0200

    Iniciando a implementação da página eprodutos.

--- src/main/resources/static/js/adicionarproduto.js
+++ src/main/resources/static/js/adicionarproduto.js
@@ -1,1 +1,25 @@
 'use strict';
+
+$(document).ready(function() {
+
+    if (localStorage.email === null || localStorage.email === undefined) {
+
+        window.location.href = '/';
+
+    } else {
+
+        if (localStorage.role !== 'enterprise') {
+            window.location.href = '/dashboard';
+        }
+
+        $('#email_label').html(localStorage.email);
+
+    }
+
+    $('#sair_btn').click(function() {
+        localStorage.removeItem('email');
+        localStorage.removeItem('role');
+        window.location.href = '/';
+    });
+
+});
