5bd37fbcc3a3d01a83b03d867f6be7256deb5af8
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-12-06 15:02:51 -0200

    Implementando paginação na página eprodutos.

--- src/main/resources/static/js/eprodutos.js
+++ src/main/resources/static/js/eprodutos.js
@@ -3,6 +3,10 @@
 $(document).ready(function() {
 
     var product = null;
+    var numPerPage = 10;
+    var noOfPages;
+    var currentPage = 1;
+    var paginationData;
 
     if (localStorage.email === null || localStorage.email === undefined) {
 
@@ -22,15 +26,33 @@ $(document).ready(function() {
             type: 'GET',
             success: function(data) {
 
-                product = data;
+                product = data.reverse();
+                noOfPages = Math.ceil(product.length / numPerPage);
+                paginationData = product.slice((currentPage - 1) * numPerPage, currentPage * numPerPage);
 
-                for (var i=0; i<product.length; i++) {
+                $('#current_page').html(currentPage);
+                $('#no_of_pages').html(noOfPages);
 
-                    var str = '<tr><td>' + product[i].brand + '</td>' +
-                                '<td>' + product[i].name + '</td>' +
-                                '<td>' + product[i].description + '</td></tr>';
+                $('#pages').append('<li><a id="previous">Previous</a></li>');
 
-                    $('#table_content').html($('#table_content').html() + str);
+                for (var i=1; i<=noOfPages; i++) {
+
+                    var str = '<li><a class="set_page">' + i + '</a></li>';
+                    $('#pages').append(str);
+
+                }
+
+                $('#pages').append('<li><a id="next">Next</a></li>');
+
+                $('#table_content').html('');
+
+                for (var i=0; i<paginationData.length; i++) {
+
+                    var str = '<tr><td>' + paginationData[i].brand + '</td>' +
+                                '<td>' + paginationData[i].name + '</td>' +
+                                '<td>' + paginationData[i].description + '</td></tr>';
+
+                    $('#table_content').append(str);
 
                 }
 
@@ -43,6 +65,74 @@ $(document).ready(function() {
         localStorage.removeItem('email');
         localStorage.removeItem('role');
         window.location.href = '/';
+    });
+
+    $('#pages').on('click', '#previous', function() {
+
+        if (currentPage > 1) {
+
+            currentPage--;
+            paginationData = product.slice((currentPage - 1) * numPerPage, currentPage * numPerPage);
+
+            $('#current_page').html(currentPage);
+            $('#table_content').html('');
+
+            for (var i=0; i<paginationData.length; i++) {
+
+                var str = '<tr><td>' + paginationData[i].brand + '</td>' +
+                            '<td>' + paginationData[i].name + '</td>' +
+                            '<td>' + paginationData[i].description + '</td></tr>';
+
+                $('#table_content').append(str);
+
+            }
+
+        }
+
+    });
+
+    $('#pages').on('click', '#next', function() {
+
+        if (currentPage < noOfPages) {
+
+            currentPage++;
+            paginationData = product.slice((currentPage - 1) * numPerPage, currentPage * numPerPage);
+
+            $('#current_page').html(currentPage);
+            $('#table_content').html('');
+
+            for (var i=0; i<paginationData.length; i++) {
+
+                var str = '<tr><td>' + paginationData[i].brand + '</td>' +
+                            '<td>' + paginationData[i].name + '</td>' +
+                            '<td>' + paginationData[i].description + '</td></tr>';
+
+                $('#table_content').append(str);
+
+            }
+
+        }
+
+    });
+
+    $('#pages').on('click', '.set_page', function() {
+
+        currentPage = parseInt($(this).html());
+        paginationData = product.slice((currentPage - 1) * numPerPage, currentPage * numPerPage);
+
+        $('#current_page').html(currentPage);
+        $('#table_content').html('');
+
+        for (var i=0; i<paginationData.length; i++) {
+
+            var str = '<tr><td>' + paginationData[i].brand + '</td>' +
+                        '<td>' + paginationData[i].name + '</td>' +
+                        '<td>' + paginationData[i].description + '</td></tr>';
+
+            $('#table_content').append(str);
+
+        }
+
     });
 
 });
