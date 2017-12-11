47065a2ab5d495a27ab5202a11c8d0307cbe91fb
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-12-11 01:16:46 -0200

    Implementando a página mensagemproduto.

--- src/main/resources/static/js/mensagemproduto.js
+++ src/main/resources/static/js/mensagemproduto.js
@@ -2,7 +2,8 @@
 
 $(document).ready(function() {
 
-    var commonUser = null;
+    var commonUser;
+    var product;
 
     if (localStorage.email === null || localStorage.email === undefined) {
 
@@ -31,12 +32,78 @@ $(document).ready(function() {
             }
         });
 
+        $.ajax({
+            url: '/api/product/' + localStorage.mensagemProdutoId,
+            contentType: 'application/json',
+            type: 'GET',
+            success: function(data) {
+                product = data;
+                $('#mensagem_empresa').val(product.enterpriseUser.tradeName);
+                $('#mensagem_produto').val(product.name);
+            }
+        });
+
     }
 
     $('#sair_btn').click(function() {
         localStorage.removeItem('email');
         localStorage.removeItem('role');
         window.location.href = '/';
+    });
+
+    $('#enviar_btn').click(function() {
+
+        var tipoMensagem = $('#mensagem_tipo').val();
+        var anonimo = $('#mensagem_anonimo').is(':checked');
+        var conteudo = $('#mensagem_conteudo').val();
+
+        if (conteudo === '' || conteudo === null) {
+
+            $('#alertMsg').html('Por favor, informe o conteúdo da mensagem.');
+            $('#alertModal').modal();
+
+        } else {
+
+            var message = {
+                messageType: tipoMensagem,
+                anonymous: anonimo,
+                content: conteudo,
+                commonUser: commonUser,
+                enterpriseUser: product.enterpriseUser,
+                product: product
+            };
+
+            $.ajax({
+                url: '/api/message',
+                contentType: 'application/json',
+                type: 'POST',
+                data: JSON.stringify(message),
+                success: function(data) {
+
+                    var msg = JSON.parse(data).msg;
+
+                    if (msg !== 'success') {
+
+                        $('#alertMsg').html(msg);
+                        $('#alertModal').modal();
+
+                    } else {
+
+                        $('#successMsg').html('Mensagem enviada com sucesso com sucesso.');
+                        $('#successModal').modal();
+
+                    }
+
+                }
+            });
+
+        }
+
+    });
+
+    $('#close_btn').click(function() {
+        localStorage.removeItem('mensagemProdutoId');
+        window.location.href = '/dashboard';
     });
 
 });
